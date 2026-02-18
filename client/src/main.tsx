6c136690bae7fb481c177ba2a2d656373bf29cfb
Eddie Joe Antonio <eddiejoe.antonio@gmail.com>
2026-02-18 13:26:52 -0500

    iFrame fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,22 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-// 1) CA template CSS (core) + optional theme
 import "@cagovweb/state-template/dist/css/cagov.core.css";
 import "@cagovweb/state-template/dist/css/colortheme-oceanside.css";
-
-// 2) Your Tailwind entry
 import "./index.css";
-
-// 3) CA template JS (dropdowns, search toggle, etc.)
 import "@cagovweb/state-template/dist/js/cagov.core.js";
 
 import App from "./App.tsx";
 
 // Report iframe height to WordPress parent
+let reportTimeout: ReturnType<typeof setTimeout>;
+
 function reportHeight() {
-  const height = document.documentElement.scrollHeight;
-  window.parent.postMessage({ type: "IFRAME_HEIGHT", height }, "*");
+  clearTimeout(reportTimeout);
+  reportTimeout = setTimeout(() => {
+    const height = document.documentElement.scrollHeight;
+    window.parent.postMessage({ type: "IFRAME_HEIGHT", height }, "*");
+  }, 150);
 }
 
 const ro = new ResizeObserver(reportHeight);
