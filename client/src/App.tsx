25a3f58be426a58dfadcf573a296e8f973267d60
Eddie Joe Antonio <eddiejoe.antonio@gmail.com>
2026-03-16 16:52:08 -0400

    Updated analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
 import ResourceFinder from "./components/ResourceFinder";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
-  return <ResourceFinder />;
+  return (
+    <>
+      <ResourceFinder />
+      <Analytics />
+    </>
+  );
 }
